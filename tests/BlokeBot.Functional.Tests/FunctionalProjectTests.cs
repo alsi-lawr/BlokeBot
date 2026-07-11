bdae8dfc996374001319b2f68caccafff17c3c9c
alsi-lawr <177320313+alsi-lawr@users.noreply.github.com>
2026-07-11 03:35:59 +0100

    build(functional): add functional project and tests

---
+++ tests/BlokeBot.Functional.Tests/FunctionalProjectTests.cs
@@ -0,0 +1,10 @@
+using Shouldly;
+using TUnit.Core;
+
+namespace BlokeBot.Functional.Tests;
+
+public sealed class FunctionalProjectTests
+{
+    [Test]
+    public void TestProject_Discovery_Succeeds() => true.ShouldBeTrue();
+}
