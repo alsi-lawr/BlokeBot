f1b569c66cf112d73e0508515bd2f3c5de49a437
alsi-lawr <177320313+alsi-lawr@users.noreply.github.com>
2026-08-09 17:37:10 +0100

    test: remove simulation utility tests

--- src/BlokeBot.Twitch.Runtime/Properties/AssemblyInfo.cs
+++ src/BlokeBot.Twitch.Runtime/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("BlokeBot.Twitch.Runtime.Tests")]
-[assembly: InternalsVisibleTo("BlokeBot.Simulation.Tests")]
 [assembly: InternalsVisibleTo("BlokeBot.Core.Tests")]
 [assembly: InternalsVisibleTo("BlokeBot.Core")]
