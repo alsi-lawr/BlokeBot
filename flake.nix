48f011e2dead71f584f9da3c55fe00a4a1fc67b4
alsi-lawr <177320313+alsi-lawr@users.noreply.github.com>
2026-08-22 16:48:37 +0100

    release: prepare v0.12.0

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
         "aarch64-linux"
         "aarch64-darwin"
       ];
-      releaseVersion = "0.11.4";
+      releaseVersion = "0.12.0";
       imageSource = "https://github.com/alsi-lawr/BlokeBot";
       imageRevision = self.rev or self.dirtyRev or "unknown";
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
